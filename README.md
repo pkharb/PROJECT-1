ccf67fa221089276fa584aa988a25c5dabb70610
pk <pradeepkharb@hotmail.com>
2018-12-03 08:56:16 -0800

    final push

---
+++ README.md
@@ -0,0 +1,16 @@
+README
+
+TECHNOLOGIES USED :-
+- HTML/CSS
+- JAVASCRIPT
+- JQUERY
+
+YOUR APPROACH :-
+- i spent too much time on CSS and kept the JQuery part for the end, but because of the mistake i made with github branching, i ended up spending a lot of time salving my project. It was very frustrating and caused a lot of stress. As a result, i could not spend as much time on jquery as i had planned. 
+
+UNSOLVED PROBLEMS :-
+- submit button styling
+- my slider is still has a few glitches
+
+BIGGEST WINS AND CHALLENGES :-
+- The mistake with github became my biggest challenge 
--- js/main.js
+++ js/main.js
@@ -1,80 +1,139 @@
 $(document).ready(function(){
-    
-    
-    var allImg = $('.img');
-    $('.next').on('click', function(){
-        
-        var firstImage = $('.first');
-        var currentImg = $('.active');
-       
-       if(currentImg[0] === allImg[allImg.length -1]){
-        
-            currentImg.removeClass('active').css('z-index', -10);
-            firstImage.addClass('active').css('z-index', 10);
-       }
-       else {
-        var nextImg = currentImg.next();
-
-        if(nextImg.length){
-            currentImg.removeClass('active').css('z-index', -10);
-            nextImg.addClass('active').css('z-index', 10);
-            
-        }
-       }
-    })
-    $('.prev').on('click', function(){
-
-        var lastImg = $('.first');
-        var currentImg = $('.active');
-        
-        
-
-        if(currentImg[0] === allImg[0]){
-            currentImg.removeClass('.active').css('z-index', -10);
-            //console.log(allImg[0]);
-            lastImg.addClass('.active').css('z-index', 10);
-            // debugger
-        }
-        else {
-            // debugger
-        var prevImg = currentImg.prev();
-
-        if(prevImg.length){
-            currentImg.removeClass('active').css('z-index', -10);
-            prevImg.addClass('active').css('z-index', 10);
-        }
-    }
-    })
-
-
-        var quotes = $(".quotes");
-        //console.log(quotes.length);
-        var quoteIndex = -1;
-    
-        function showNextQuote() {
-            ++quoteIndex;
-            quotes.eq(quoteIndex % quotes.length)
-                .fadeIn(2000)
-                .delay(2000)
-                .fadeOut(2000, showNextQuote);
-        }
-    
-        showNextQuote();
-
-        var writer = $(".writer");
-        var writerIndex = -1;
-    
-        function showNextWriter() {
-            ++writerIndex;
-            writer.eq(writerIndex % writer.length)
-                .fadeIn(2000)
-                .delay(2000)
-                .fadeOut(2000, showNextWriter);
-        }
-    
-        showNextWriter();
-    
    
+    // SLIDER
+    // SLIDER
+    var speed = 500;           //speed of fade
+    var autoSlide = true;       //auto slider option
+    var autoslide_speed = 4000; //auto slider speed
+
+
+    // add initial active class
+    $('.slide').first().addClass('active');
+
+    // hide all slides
+    $('.slide').hide();
+
+    // show first slide
+    $('.active').show();
+
+    $('#next').on('click', function(){
+        $('.active').removeClass('active').addClass('prevActive');
+
+        if($('.prevActive').is(':last-child')){
+            $('.slide').first().addClass('active');
+        }
+        else{
+            $('.prevActive').next().addClass('active');
+        }
+        $('.prevActive').removeClass('prevActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    });
+
+    $('#prev').on('click', function(){
+        $('.active').removeClass('active').addClass('prevActive');
+        //console.log($(".prevActive"));
+
+        if($('.prevActive').is(':first-child')){
+            $('.slide').last().addClass('active');
+        }
+        else{
+            $('.prevActive').prev().addClass('active');
+        }
+        $('.prevActive').removeClass('prevActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    });
+    
+    var speed = 500;           //speed of fade
+    var autoSlide = true;       //auto slider option
+    var autoslide_speed = 4000; //auto slider speed
+    if(autoSlide === true){
+        setInterval(function(){
+            $('.active').removeClass('active').addClass('prevActive');
+
+        if($('.prevActive').is(':last-child')){
+            $('.slide').first().addClass('active');
+        }
+        else{
+            $('.prevActive').next().addClass('active');
+        }
+        $('.prevActive').removeClass('prevActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+        }, autoslide_speed);
+    }
+
+    // var mySlides = $('.slide');
+    // var slideIndex = -1;
+    // console.log(mySlides);
+    // function showNextSlide() {
+    //     console.log(quoteIndex);
+    //     ++slideIndex;
+    //     console.log(quoteIndex);
+    //     mySlides.eq(slideIndex % mySlides.length)
+    //         .fadeIn(2000)
+    //         .delay(2000)
+    //         .fadeOut(2000, showNextSlide);
+    // }
+    // showNextSlide();
+
+    // TESTIMONIALS
+    // TESTIMONIALS
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+
+    function showNextQuote() {
+        ++quoteIndex;
+        console.log(quoteIndex);
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextQuote);
+    }
+    showNextQuote();
+
+    var writer = $(".writer");
+    var writerIndex = -1;
+
+    function showNextWriter() {
+        ++writerIndex;
+        writer.eq(writerIndex % writer.length)
+            .fadeIn(2000)
+            .delay(2000)
+            .fadeOut(2000, showNextWriter);
+    }
+    showNextWriter();
+
+    // FORM VALIDATION
+    // FORM VALIDATION
+
+    // VALIDATE NAME
+
+    // $(':submit').on('click', function(){
+    //     var inputName = $('#fname').text();
+    //     console.log(inputName);
+    // if(inputName === ""){
+    //     alert("name");
+    // }
+
+    // var inputEmail = $('#femail').text();
+
+    // if(inputEmail === ""){
+    //     alert("email");
+    // }
+    // if(inputEmail.filter(xx => xx !== "@")){
+    //     console.log("enter correct Email");
+    // }
+
+    // var inputPhone = $('#fphone').text();
+    // if(inputPhone === ""){
+    //     alert("phone");
+    // }
+    // })
+
+
+    
 });
 
 // RESPONSIVENESS
